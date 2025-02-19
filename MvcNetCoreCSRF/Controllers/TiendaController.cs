87de03e8598948c9b017b589200823685c82b075
serraguti <pacoserranox@gmail.com>
2025-02-19 11:52:56 +0100

    Add project files.

---
+++ MvcNetCoreCSRF/Controllers/TiendaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcNetCoreCSRF.Controllers
+{
+    public class TiendaController : Controller
+    {
+        public IActionResult Productos()
+        {
+            //SI EL USUARIO NO SE HA VALIDADO TODAVIA
+            //LO LLEVAMOS A DENIED
+            if (HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("Denied", "Managed");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Productos(string direccion
+            , string[] producto)
+        {
+            if (HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("Denied", "Managed");
+            }
+            else
+            {
+                //MEDIANTE TEMPDATA SE ALMACENA LA INFORMACION
+                //PARA SER ENVIADA A OTROS CONTROLLERS O 
+                //METODOS IACTIONRESULT EN LAS REDIRECCIONES
+                TempData["PRODUCTOS"] = producto;
+                TempData["DIRECCION"] = direccion;
+                return RedirectToAction("PedidoFinal");
+            }
+        }
+
+        public IActionResult PedidoFinal()
+        {
+            //AQUI NECESITO LOS PRODUCTOS Y LA DIRECCION
+            //DEL METODO POST DE PRODUCTOS.  COMO LOS RECUPERO AQUI??
+            string[] productos = TempData["PRODUCTOS"]
+                as string[];
+            ViewData["DIRECCION"] = TempData["DIRECCION"];
+            return View(productos);
+        }
+    }
+}
---
+++ MvcNetCoreCSRF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcNetCoreCSRF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
